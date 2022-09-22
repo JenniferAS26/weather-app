3049134176d54eaf3eb98a84496e15fb67f8002b
JenniferAS26 <90392630+JenniferAS26@users.noreply.github.com>
2022-09-22 14:47:01 -0500

    Fix FavoriteContainer style

--- src/components/FavoriteContent.jsx
+++ src/components/FavoriteContent.jsx
@@ -1,63 +1,23 @@
 import React from 'react';
 import '@styles/FavoriteContent.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeartCrack } from '@fortawesome/free-solid-svg-icons';
 
 
 const FavoriteContent = () => {
   return (
     <div className="FavoriteContent">
       <div  className="FavoriteContent-card">
-        <h3 className="city">City name</h3>
-        <p className="temp">35
+        <div className="top-section">
+          <h3 className="top-section__city">City name</h3>
+          <button className="top-section__addFav" type="button">
+              <FontAwesomeIcon icon={faHeartCrack} className='brokenHeart' />
+              </button>
+        </div>
+        <p className="FavoriteContent-card__temp">
+          35
         <span>°C</span>
         </p>
-        <button className="addFav" type="button">
-          Fav
-        </button>
-      </div>
-      <div  className="FavoriteContent-card">
-        <h3 className="city">City name</h3>
-        <p className="temp">35
-        <span>°C</span>
-        </p>
-        <button className="addFav" type="button">
-          Fav
-        </button>
-      </div>
-      <div  className="FavoriteContent-card">
-        <h3 className="city">City name</h3>
-        <p className="temp">35
-        <span>°C</span>
-        </p>
-        <button className="addFav" type="button">
-          Fav
-        </button>
-      </div>
-      <div  className="FavoriteContent-card">
-        <h3 className="city">City name</h3>
-        <p className="temp">35
-        <span>°C</span>
-        </p>
-        <button className="addFav" type="button">
-          Fav
-        </button>
-      </div>
-      <div  className="FavoriteContent-card">
-        <h3 className="city">City name</h3>
-        <p className="temp">35
-        <span>°C</span>
-        </p>
-        <button className="addFav" type="button">
-          Fav
-        </button>
-      </div>
-      <div  className="FavoriteContent-card">
-        <h3 className="city">City name</h3>
-        <p className="temp">35
-        <span>°C</span>
-        </p>
-        <button className="addFav" type="button">
-        Fav
-        </button>
       </div>
     </div>
   );
